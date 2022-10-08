59d3700fbf92d3906509805ee01441a8369a3318
Alan <alancabrera@gmail.com>
2022-10-08 18:35:54 -0500

    First Committ

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div c>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
