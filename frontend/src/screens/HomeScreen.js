d0f0a3dfc3a0c4a3e3be2fec8fbdfb7b45981a0e
Alan <alancabrera@gmail.com>
2022-10-15 12:25:10 -0500

    feat: added proxy to enable front end to get data from back end using axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+//import data from '../data'; is no longer used because it is static
 
 function HomeScreen() {
+  /*useState allows us to update an array of two variables
+  with statements that are local to this component/function*/
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      /* this uses axios to send a request to the api and then
+      store the returned value*/
+      const result = await axios.get('/api/products');
+      setProducts(result.data); //result.data has the products from the backend.
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
         {/* the following is a JSX (javascript xml) */}
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
