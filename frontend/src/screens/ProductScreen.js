13ac033efca84aaa2533fcf29f7c40a2cd829c1f
Alan <alancabrera@gmail.com>
2022-10-23 22:33:15 -0500

    feat: added bootstrap columns to display product info

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useEffect, useReducer } from 'react';
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Rating from '../components/Rating';
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -49,7 +55,63 @@ function ProductScreen() {
   ) : error ? (
     <div>{error}</div>
   ) : (
-    <div>{product.name}</div>
+    <div>
+      <Row>
+        {/*This column is for the product image*/}
+        <Col md={6}>
+          <img
+            className="img-large"
+            src={product.image}
+            alt={product.name}
+          ></img>
+        </Col>
+        {/*This column is for the product information
+        it will use the ListGroup. Flush removes border.*/}
+        <Col md={3}>
+          <ListGroup vairant="flush">
+            <ListGroup.Item>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Description: <p>{product.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup vairant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg="success">In Stock</Badge>
+                      ) : (
+                        <Badge bg="danger">Unavailable</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>{' '}
+        </Col>
+      </Row>
+    </div>
   );
 }
 
